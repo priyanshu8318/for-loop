3956a9208f106bcd6dc042c36d58ab808ce15890
priyanshu8318 <103734794+priyanshu8318@users.noreply.github.com>
2022-04-16 19:15:36 +0530

    Add files via upload

---
+++ prime num or not.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+  cout << "Enter the value of n" << endl;
+  cin >> n;
+  int i;
+  for (int i=2; i<n; i++){
+    if (n%2==0){
+      cout << " number is not prime number"  <<endl;
+      break;
+    }
+    else{
+      cout << "number is prime number" << endl;
+      break;
+      
+    }
+      
+    }
+    
+    
+    
+  }
+  
+
+  
